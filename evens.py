6331f3d5560f954e70b87963f167559383a05d71
Alexandru Ursu <bodhipankh@yahoo.com>
2023-08-01 00:04:47 +0000

    Generate test code and link files

--- evens.py
+++ evens.py
@@ -9,5 +9,7 @@ def even_number_of_evens(numbers):
 
     return None
 
+# print(even_number_of_evens(5))
+
 if __name__ == "__main__":
     print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -5,6 +5,11 @@ from evens import even_number_of_evens
 class TestEvens(unittest.TestCase):
     pass
 
+    # def test_function_returns_True(self):
+    #     self.assertTrue(even_number_of_evens([]))
+
 
 if __name__ == "__main__":
     unittest.main()
+
+# unittest.main()
